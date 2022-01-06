2b20197325b3bfc94980e03d9b1a0f61ddf2b470
Peter Jannesen <peter@jannesen.com>
2022-01-06 18:42:37 +0100

    CleanTarget retry delete

--- Jannesen.VisualStudioExtension.NBuildProject.Build/CleanTargetTree.cs
+++ Jannesen.VisualStudioExtension.NBuildProject.Build/CleanTargetTree.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
+using System.Threading;
 using Microsoft.Build.Framework;
 using Jannesen.VisualStudioExtension.NBuildProject.Build.Library;
 
@@ -16,31 +17,52 @@ namespace Jannesen.VisualStudioExtension.NBuildProject.Build
         {
             string      directory = FullFileName(TargetDirectory);
 
-            if (Directory.Exists(directory)) {
-                Log.LogMessage(MessageImportance.High, "Cleanup tree: " + directory);
-                _cleanupDirectory(directory);
+            if (!Directory.Exists(directory)) {
+                return true;
             }
 
-            return true;
+            Log.LogMessage(MessageImportance.High, "Cleanup tree: " + directory);
+            for (var i = 0; i < 3 ; i++) {
+                if (_cleanupDirectory(directory, false)) {
+                    return true;
+                }
+                Log.LogWarning("Cleanup failed. Retry...");
+                Thread.Sleep(1000);
+            }
+
+            return _cleanupDirectory(directory, true);
         }
 
-        private                 bool                _cleanupDirectory(string directory)
+        private                 bool                _cleanupDirectory(string directory, bool logError)
         {
-            List<FileSystemInfo>    entrys = new List<FileSystemInfo>((new DirectoryInfo(directory)).EnumerateFileSystemInfos());
-            int                     n = 0;
+            List<FileSystemInfo>    entrys;
+
+            try {
+                entrys = new List<FileSystemInfo>((new DirectoryInfo(directory)).EnumerateFileSystemInfos());
+            }
+            catch(DirectoryNotFoundException) {
+                return true;
+            }
+
+            bool    rtn = true;
 
             foreach (FileSystemInfo entry in entrys) {
                 if ((entry.Attributes & (FileAttributes.Directory | FileAttributes.Device | FileAttributes.ReparsePoint | FileAttributes.Hidden | FileAttributes.System)) == FileAttributes.Directory) {
                     string  path = directory + "\\" + entry.Name;
 
-                    if (_cleanupDirectory(path)) {
+                    if (_cleanupDirectory(path, logError)) {
                         try {
                             Directory.Delete(path);
-                            ++n;
                         }
                         catch(Exception err) {
-                            throw new BuildException("Can't delete directory: " + path, err);
+                            if (logError) {
+                                Log.LogError("Can't delete directory '" + path + "': " + err.Message);
+                            }
+                            rtn = false;
                         }
+                    }
+                    else {
+                        rtn = false;
                     }
                 }
             }
@@ -53,13 +75,15 @@ namespace Jannesen.VisualStudioExtension.NBuildProject.Build
                         File.Delete(path);
                     }
                     catch(Exception err) {
-                        throw new BuildException("Can't delete file: " + path, err);
+                        if (logError) {
+                            Log.LogError("Can't delete file '" + path + "': " + err.Message);
+                        }
+                        rtn = false;
                     }
-                    ++n;
                 }
             }
 
-            return n == entrys.Count;
+            return rtn;
         }
     }
 }
