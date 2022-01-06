2cff5e2bfcd4b2a37e884592114687c28edcbbf2
Peter Jannesen <peter@jannesen.com>
2022-01-06 18:41:41 +0100

    Update to VS 2022

--- Jannesen.VisualStudioExtension.NBuildProject.Build/GlobalSuppressions.cs
+++ Jannesen.VisualStudioExtension.NBuildProject.Build/GlobalSuppressions.cs
@@ -1,11 +1,3 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿using System.Diagnostics.CodeAnalysis;
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design",        "CA1031:Do not catch general exception types")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance",   "CA1819:Properties should not return arrays")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability",   "CA2007:Consider calling ConfigureAwait on the awaited task")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style",         "IDE1006:Naming Styles")]
+[assembly: SuppressMessage("Style",         "IDE1006:Naming Styles")]
--- NBuildProject.cmd
+++ NBuildProject.cmd
@@ -1,4 +1,4 @@
 set NBuildProjectBuild=%~dp0Jannesen.VisualStudioExtension.NBuildProject\bin\
 set TypedTSqlBuild=%~dp0../TypedTSql/Jannesen.VisualStudioExtension.TypedTSql.Build\bin\
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" NBuildProject.sln
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe" NBuildProject.sln
