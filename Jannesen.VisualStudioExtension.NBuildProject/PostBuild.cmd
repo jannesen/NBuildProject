e6b5eaed52c3b0a50be2a55cb17d17d99e1ec30a
Peter Jannesen <peter@jannesen.com>
2025-11-25 13:04:50 +0100

    Update csproj file to new format

--- Jannesen.VisualStudioExtension.NBuildProject/PostBuild.cmd
+++
@@ -1,6 +0,0 @@
-
-set target=%~dp0..
-set source=%~dp0bin\Jannesen.VisualStudioExtension.NBuildProject.vsix
-
-if not exist "%target%" mkdir "%target%"
-copy "%source%" "%target%\NBuildProject.vsix"
--- Jannesen.VisualStudioExtension.NBuildProject/VSPackage.cs
+++ Jannesen.VisualStudioExtension.NBuildProject/VSPackage.cs
@@ -5,15 +5,16 @@ using Microsoft.VisualStudio.Shell;
 
 namespace Jannesen.VisualStudioExtension.NBuildProject
 {
+    // VSPackage.Version is generated during build.
+
     [Guid(VSPackage.PackageGuidString)]
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", VSPackage.Version, IconResourceID = 400)]
     [Description("NBuildProject Visual Studio Extension.")]
     [ProvideAutoLoad(Microsoft.VisualStudio.Shell.Interop.UIContextGuids80.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
-    public sealed class VSPackage: AsyncPackage
+    public sealed partial class VSPackage: AsyncPackage
     {
         public      const       string                              PackageGuidString   = "24077884-E16E-4CC2-937F-7CA74CCE53AE";
-        public      const       string                              Version             = "1.10.02.000";        //@VERSIONINFO
 
         public                                                      VSPackage()
         {
